88c3556b7600f731c6272a3f36f27880b3bd7043
Shallange <jimmy.kroneld@hotmail.com>
2026-09-05 15:01:17 +0200

    feat: add local Wake-on-LAN support

--- esp32/wakeBridgeESP32/secrets.example.h
+++ esp32/wakeBridgeESP32/secrets.example.h
@@ -2,3 +2,10 @@
 
 #define WIFI_SSID "Your_WiFi_Name"
 #define WIFI_PASSWORD "Your_WiFi_Password"
+
+// Replace this with the MAC address of the pc you want to wake.
+// Format: 0xAA, 0xBB, 0xCC, 0xDD, 0xEE, 0xFF
+const uint8_t PC_MAC[6] = {
+  0x00, 0x00, 0x00,
+  0x00, 0x00, 0x00
+};
--- esp32/wakeBridgeESP32/wakeBridgeESP32.ino
+++ esp32/wakeBridgeESP32/wakeBridgeESP32.ino
@@ -1,5 +1,11 @@
 #include <WiFi.h>
+#include <WiFiUdp.h>
 #include "secrets.h"
+
+#define WOL_BROADCAST_IP "192.168.1.255"
+#define WOL_PORT 9
+
+WiFiUDP udp;
 
 void setup() {
   Serial.begin(115200);
@@ -19,6 +25,37 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // Temporary local test:
+  // send a WoL packet when 'w' is entered in the Serial Monitor.
+  if (Serial.available()) {
+    char command = Serial.read();
 
+    if(command == 'w') {
+      sendWakePacket();
+    }
+  }
+}
+
+void sendWakePacket() {
+  // A Wake-on-LAN magic packet contains:
+  // 6 bytes of 0xFF followed by the target MAC adress repeated 16 times.
+  uint8_t packet[102];
+
+  // Fill the first 6 bytes with 0xFF
+  for (int i = 0; i < 6; i++){
+    packet[i] = 0xFF;
+  }
+
+  // Reapeat the target PC's MAC address 16 times.
+  for (int i = 1; i <= 16; i++) {
+    for (int j = 0; j < 6; j++) {
+      packet[i * 6 + j] = PC_MAC[j];
+    }
+  }
+  //Broadcast the magic packet on the local network
+  udp.beginPacket(WOL_BROADCAST_IP, WOL_PORT);
+  udp.write(packet, sizeof(packet));
+  udp.endPacket();
+
+  Serial.println("Wake-on-Lan packet sent");
 }
